cad8c9e8042040e6302bc3c3959cbeb68f08ecb6
Ilya <i.yakubov.pro@gmail.com>
2022-07-27 22:45:59 +0300

    Добавлен метод загрузки сотрудников из таблицы Employee

--- src/main/java/controllers/EmployeeController.java
+++ src/main/java/controllers/EmployeeController.java
@@ -1,11 +1,13 @@
 package controllers;
 
-import domains.tables.records.EmployeesRecord;
+import com.sun.javafx.collections.ObservableListWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import org.jooq.Record1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static services.EmployeesService.EMPLOYEES_SERVICE;
@@ -13,7 +15,7 @@ import static services.EmployeesService.EMPLOYEES_SERVICE;
 public class EmployeeController {
 
     @FXML
-    private ComboBox<EmployeesRecord> comboBoxEmployees;
+    private ComboBox<String> comboBoxEmployees;
 
     @FXML
     void onClickButtonStart() {
@@ -24,10 +26,8 @@ public class EmployeeController {
     void initialize() {
         assert comboBoxEmployees != null : "fx:id=\"comboBoxEmployees\" was not injected: check your FXML file 'login.fxml'.";
 
-        List<EmployeesRecord> employeesRecord = EMPLOYEES_SERVICE.getEmployeesFromDB();
+        List<String> employees = EMPLOYEES_SERVICE.getEmployeesFromDB();
 
-        ObservableList<EmployeesRecord> employees = FXCollections.observableArrayList(employeesRecord);
-
-        comboBoxEmployees.setItems(employees);
+        comboBoxEmployees.setItems(FXCollections.observableArrayList(employees));
     }
 }
--- src/main/java/services/EmployeesService.java
+++ src/main/java/services/EmployeesService.java
@@ -1,9 +1,18 @@
 package services;
 
+import com.sun.javafx.collections.ObservableListWrapper;
+import database.ConnectionURL;
 import domains.Tables;
 import domains.tables.Employees;
 import domains.tables.records.EmployeesRecord;
+import javafx.collections.ObservableList;
+import org.jooq.DSLContext;
+import org.jooq.Record1;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +26,25 @@ public class EmployeesService {
         // иначе - EmployeeService
     }
 
-    public List<EmployeesRecord> getEmployeesFromDB() {
-        List<EmployeesRecord> employees = new ArrayList<>();
+    public List<String> getEmployeesFromDB() {
+        List<String> employees = new ArrayList<>();
+        Result<Record1<String>> resultSet;
+        DSLContext dsl;
+        try {
+            dsl = DSL.using(ConnectionURL.getConnection());
+            resultSet = dsl.select(Employees.EMPLOYEES.FIRST_NAME)
+                    .from(Employees.EMPLOYEES)
+                    .orderBy(Employees.EMPLOYEES.EMPLOYEE_ID).fetch();
 
-        employees.add(new EmployeesRecord());
+            List<Record1<String>> result = new ArrayList<>();
+            result.addAll(resultSet);
+
+            for (Record1<String> stringRecord1 : result) {
+                employees.add(stringRecord1.value1());
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
         return employees;
     }
